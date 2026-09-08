b9606605c691c9a969016ad6932fcce8deb5ac8a
cane-sugar-grove <258354248+cane-sugar-grove@users.noreply.github.com>
2026-09-07 22:32:47 -0700

    dialogue updates

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const SHORT_HAND =  //shorthand so i don't clog up stuff with big blockos of textos
 {
     "01auth_note" : 
-    "So... we're getting further and further away from the sun. \
+    "It's confirmed, we're getting further and further away from the sun. \
      The nerds in R&D are still trying to figure out how to put us back or how to make an artificial sun. \
      Even with all the funding in the world, we're still f[REDACTED]ed.  To make things worse, \
      The World Machine keeps breaking, and I can't figure out why.  I think the only course of action is to implement \
@@ -9,7 +9,9 @@ const SHORT_HAND =  //shorthand so i don't clog up stuff with big blockos of tex
      I'll tweak my old antivirus to search for errors during world implementation \
      and throw it in there. \
      <br> <br> <br> \
-     Anyways, I'm going to take \"the kids\" out for ice cream. <br>  The things you can do with biomimicry, huh?"
+     Anyways, I'm going to take \"the kids\" out for ice cream. <br>  The things you can do with biomimicry, huh?  They can even taste things now!"
+
+
 }
 
 const FLAG_KEYS =
@@ -25,7 +27,6 @@ function submFlag()
 
     if(FLAG_KEYS[flag.value])
     {
-        console.log("hello!");
         window.location.href = FLAG_KEYS[flag.value];
     }
 }
@@ -35,8 +36,6 @@ function reDirect(link)
 {
     window.location.href = 'https://'.concat(link);
 }
-
-
 
 function displayPasswordPrompt(password, followFunction)
 {
@@ -49,8 +48,6 @@ function displayPasswordPrompt(password, followFunction)
             console.log("2")
             
             let stringSplit = followFunction.split("|");
-
-            console.log(stringSplit, " - hello!")
             
             if(stringSplit[0] == "dispText")
             {
